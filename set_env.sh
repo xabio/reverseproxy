d4a0ebce232f1b46d741562bba3487de9666a378
Xabi <Xabi@DESKTOP-OMEN>
2025-01-03 16:05:44 +0100

    INITIAL

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+setx POSTGRES_USERNAME justin
+setx POSTGRES_PASSWORD Pelicano1
+setx POSTGRES_HOST cdr2.clr2pnezpd17.us-east-1.rds.amazonaws.com
+setx POSTGRES_DB postgres
+setx AWS_BUCKET xabi-udacity
+setx AWS_REGION us-east-1
+setx AWS_PROFILE default
+setx JWT_SECRET testing
+setx URL http://localhost:8100
